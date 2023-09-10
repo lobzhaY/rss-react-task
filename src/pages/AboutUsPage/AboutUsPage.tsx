02bdf3fa19ee8c2ccae3aed433ac7de5bd2aa0f5
Yuliya Lobzha <yulia.lobzha@gmail.com>
2023-09-10 16:08:56 +0300

    bugfix: add description of about us page

--- src/pages/AboutUsPage/AboutUsPage.tsx
+++ src/pages/AboutUsPage/AboutUsPage.tsx
@@ -3,5 +3,22 @@ import React from 'react';
 import TitleComponent from '../../components/Title/TitleComponent';
 
 export default function AboutUsPage() {
-  return <TitleComponent name="About us" />;
+  return (
+    <>
+      <TitleComponent name="About us" />
+      <div className="about-container">
+        <p>
+          OJJO is a pet project created as part of training at Rolling Scopes School. Initially it
+          was conceived as a website for a jewelry store. Hence some references in the design style.
+          But a little later, in the API module, the product concept changed.
+        </p>
+        <p>
+          The project is written in React using TypeScript and the Vite builder. And it fully
+          complies with the given technical specifications. The project has implemented SSR, test
+          coverage is more than 80%, there are no Eslint errors or warnings.
+        </p>
+        <h3>I hope you enjoy this work</h3>
+      </div>
+    </>
+  );
 }
